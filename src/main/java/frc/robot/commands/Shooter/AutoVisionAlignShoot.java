b00016a9ad7a52527f8184f3bb6f674182ccf6ba
JOHN BOYNTON <john.boynton@rsu25.org>
2024-04-05 09:18:03 -0400

    match auto vision align

--- src/main/java/frc/robot/commands/Shooter/AutoVisionAlignShoot.java
+++ src/main/java/frc/robot/commands/Shooter/AutoVisionAlignShoot.java
@@ -75,7 +75,7 @@ public class AutoVisionAlignShoot extends Command {
         // use ty to calculate shooter angle
         // Eggo (-0.009811884*ty*ty) + (0.740631*ty) + (17);  // 18.3463
         //yShooterAngle = (-0.00280493*ty*ty) + (0.986053*ty) + (22.5) + shooterAddValue;  // 21.3444 was 22.5
-        yShooterAngle = (-0.0104986*ty*ty) + (0.80866*ty) + (18) + shooterAddValue;
+        yShooterAngle = (-0.0104986*ty*ty) + (0.80866*ty) + (18.2) + shooterAddValue;
 
         // average data from both equations
         shooterAngle = ((yShooterAngle + yShooterAngle) / 2) - xSpeed;  // is this going the right way and is it the right value
